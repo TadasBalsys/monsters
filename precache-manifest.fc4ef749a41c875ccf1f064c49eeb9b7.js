c711a6b7534605bded70ef96907137dc7aa0cecc
Tadas Balsys <balsys.tadas@gmail.com>
2019-07-31 23:20:13 +0300

    Updates

---
+++ precache-manifest.fc4ef749a41c875ccf1f064c49eeb9b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4145dff4207d67d87f5e35cee1f7d6",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "34ad2ca9dfdffec60681",
+    "url": "/monsters/static/css/main.49f8727f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "34ad2ca9dfdffec60681",
+    "url": "/monsters/static/js/main.87f585f5.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe0c29d0b5b9cb6bf08af1a0ec1f8ea6.js"
+  "/monsters/precache-manifest.fc4ef749a41c875ccf1f064c49eeb9b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
